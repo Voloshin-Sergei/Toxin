4ecf6210a848c90a7ee58eaf1b6e0d5c1d97183a
Voloshin Sergei <4voloshin@gmail.com>
2023-03-20 16:12:48 +0300

    feat: add linters and prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,71 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const FileManagerPlugin = require('filemanager-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const FileManagerPlugin = require("filemanager-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index.js'),
+  entry: path.join(__dirname, "src", "index.js"),
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'index.[contenthash].js',
-    assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
+    path: path.join(__dirname, "dist"),
+    filename: "index.[contenthash].js",
+    assetModuleFilename: path.join("images", "[name].[contenthash][ext]"),
   },
   module: {
     rules: [
       {
         test: /\.(woff2?|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.js$/,
-        use: 'babel-loader',
+        use: "babel-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.pug$/,
-        loader: 'pug-loader',
+        loader: "pug-loader",
       },
       {
         test: /\.(scss|css)$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.(png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.svg$/,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: path.join('icons', '[name].[contenthash][ext]'),
+          filename: path.join("icons", "[name].[contenthash][ext]"),
         },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'template.pug'),
-      filename: 'index.html',
+      template: path.join(__dirname, "src", "template.pug"),
+      filename: "index.html",
     }),
     new FileManagerPlugin({
       events: {
         onStart: {
-          delete: ['dist'],
+          delete: ["dist"],
         },
       },
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
+      filename: "[name].[contenthash].css",
     }),
   ],
   devServer: {
-    watchFiles: path.join(__dirname, 'src'),
+    watchFiles: path.join(__dirname, "src"),
     port: 9000,
   },
   optimization: {
@@ -70,10 +75,10 @@ module.exports = {
           implementation: ImageMinimizerPlugin.imageminMinify,
           options: {
             plugins: [
-              ['gifsicle', { interlaced: true }],
-              ['jpegtran', { progressive: true }],
-              ['optipng', { optimizationLevel: 5 }],
-              ['svgo', { name: 'preset-default' }],
+              ["gifsicle", { interlaced: true }],
+              ["jpegtran", { progressive: true }],
+              ["optipng", { optimizationLevel: 5 }],
+              ["svgo", { name: "preset-default" }],
             ],
           },
         },
